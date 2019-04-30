19d247bed4a48a013afa37b9173d4129165eb8bd
Mohmed BEL <mohamed.bel@achatpublic.com>
2019-04-30 11:51:18 +0200

    Config Spring Data Source

--- redtool/controller/src/main/java/com/ach/redtool/controller/conf/mvc/HibernateConfig.java
+++ redtool/controller/src/main/java/com/ach/redtool/controller/conf/mvc/HibernateConfig.java
@@ -1,11 +1,13 @@
 package com.ach.redtool.controller.conf.mvc;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ComponentScan(basePackages = { "com.ach.redtool.repository.*" })
+@ComponentScan(basePackages ="com.ach.redtool.repository.*")
+@EntityScan("com.ach.redtool.repository.domain.*")
 @PropertySource("classpath:application.properties")
 public class HibernateConfig {
 	
