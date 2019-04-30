82adc223fff5009053a74fd11ae7d90cc373baad
Mohmed BEL <mohamed.bel@achatpublic.com>
2019-04-30 12:20:18 +0200

    externalisation data source

--- redtool/controller/src/main/java/com/ach/redtool/controller/ControllerApplication.java
+++ redtool/controller/src/main/java/com/ach/redtool/controller/ControllerApplication.java
@@ -1,14 +1,10 @@
 package com.ach.redtool.controller;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.embedded.EmbeddedWebServerFactoryCustomizerAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -16,7 +12,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 //@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EnableJpaRepositories(basePackages="com.ach.redtool.repository.*")
-@PropertySource("classpath:application.properties")
 @ComponentScan({"com.ach.redtool.controller.*","com.ach.redtool.service.*","com.ach.redtool.repository.*"})
 public class ControllerApplication extends SpringBootServletInitializer  {
 
